d3ee249f97e8c9f07dd685a4ad7cbcbb126b1942
reffya <allhailreffya@gmail.com>
2024-08-22 18:34:11 +0200

    added link with mongoDB and the pintefame command

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from twitchio.ext import commands
 import random
 import time
 import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+uri = os.getenv("MONGO_URL")
+client : MongoClient = MongoClient(uri)
+database = client.get_database("twitch_chatbot")
+users = database.get_collection("user")
 
 
 def get_minutes(a,b):
@@ -9,14 +16,14 @@ def get_minutes(a,b):
     return c / 120
 
 alcool_rates = {}
-
+load_dotenv()
 bot = commands.Bot(
-    token= os.environ["TOKEN"],
-    client_id=os.environ["CLIENT_ID"],
-    nick="Reffyatron",
-    prefix="!",
-    initial_channels=['superfatyoh2']
-)
+        token= os.getenv("TOKEN"),
+        client_id=os.getenv("CLIENT_ID"),
+        nick="Reffyatron",
+        prefix="!",
+        initial_channels=['superfatyoh2','reffya']
+    )
 
 
 @bot.event
@@ -24,24 +31,42 @@ async def event_message(ctx):
     print(ctx.author.name)
     print(ctx.content)
 
+@bot.command(name='pintefame')
+async def pintefame(ctx):
+    aggregation = [{ '$sort': { 'commands.pinte.used': -1 } },{'$limit' : 10}]
+
+    leaderboard = users.aggregate(aggregation)
+    output = ""
+    index = 1
+
+    for user in leaderboard:
+        output += " " + str(index) + ". " + user["name"] + ": " + str(user["commands"]["pinte"]["used"]) + " pinte(s) |"
+        index = index + 1
+
+    output = output[:-1]
+    
+    
+    await ctx.reply(output)
 
 @bot.command(name='turbopinte')
-async def test_command(ctx):
+async def turbopinte(ctx):
+    search_query = {"$or" : [{"name" : ctx.author.name},{"id": ctx.author.id}]}
     boost = random.randint(5,20)
-    res = [boost,time.time()]
-    author = ctx.author.name
-    if author in alcool_rates:
-        data = alcool_rates.get(author)
-        new_rate = data[0] * (0.9**get_minutes(time.time(),data[1])) + boost
-        res = [new_rate, time.time()]
-    alcool_rates.update({author : res})
-    print(author)
-    print("tu es bourré à " + str(res) +"%")
-    await ctx.reply("tu es bourré à " + str(int(res[0])) +"%")
-
-    
+    author = users.find_one(search_query)
+    new_rate = 0
+    used = 1
+    if not author == None:
+        data = author["commands"]["pinte"]
+        print(data)
+        new_rate = data["rate"] * (0.9**get_minutes(time.time(),data["last_drank"])) + boost
+        used = data["used"]+1
+        users.update_one(search_query, {'$set' : {"name" : ctx.author.name, "id" : ctx.author.id, 'commands' : { 'pinte' : { 'used' : used, 'rate' : new_rate, 'last_drank' : time.time()}}}})
+    else:
+        new_rate = boost
+        users.insert_one({"name" : ctx.author.name, "id" : ctx.author.id, "commands" : { "pinte" : {"used" : 1, "rate" : new_rate, "last_drank" : time.time()}}})
         
+    await ctx.reply("Voilà ton verre, tu es bourré à " + str(int(new_rate)) +"%. C'est déjà la " + str(used) + "e fois que tu bois")
     
-
 if __name__ == '__main__':
     bot.run()
+    client.close()
